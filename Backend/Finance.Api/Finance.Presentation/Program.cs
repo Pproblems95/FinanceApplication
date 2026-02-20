8ccd785af2347780e922cb40f06d50f66e1c0bc3
mikiHeadrest <miguelangelopezlopezll@gmail.com>
2026-02-19 18:58:59 -0800

    Proyecto unificado funcinando

--- Backend/Finance.Api/Finance.Presentation/Program.cs
+++ Backend/Finance.Api/Finance.Presentation/Program.cs
@@ -3,31 +3,39 @@ using Vite.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddViteServices();
+builder.Services.AddRazorPages();
+
+builder.Services.AddViteServices(options =>
+{
+    options.Server.AutoRun = true;
+    options.Server.Https = true;
+    options.Server.UseReactRefresh = true;
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseViteDevelopmentServer();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.MapRazorPages();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebSockets();
+    // Use Vite Dev Server as middleware.
+    app.UseViteDevelopmentServer(true);
+}
 
 app.Run();
--- Backend/Finance.Api/Finance.Presentation/vite.config.ts
+++ Backend/Finance.Api/Finance.Presentation/vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig, type UserConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import fs from 'fs'
-import path from 'path'
+/**
+ * Name: vite.config.ts
+ * Description: Vite configuration file
+ */
 
-// @ts-ignore
-import appsettings from "./appsettings.json"
-// @ts-ignore
-import appsettingsDev from "./appsettings.Development.json"
+import { type UserConfig, defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { spawn } from 'child_process';
+import fs from 'fs';
+import path from 'path';
 
-import * as process from "process"
-import { spawn } from 'child_process'
-
+// Get base folder for certificates.
 const baseFolder =
     process.env.APPDATA !== undefined && process.env.APPDATA !== ''
         ? `${process.env.APPDATA}/ASP.NET/https`
         : `${process.env.HOME}/.aspnet/https`;
 
+// Generate the certificate name using the NPM package name
 const certificateName = process.env.npm_package_name;
+
+// Define certificate filepath
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+// Define key filepath
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
 // Pattern for CSS files
@@ -25,99 +28,90 @@ const cssPattern = /\.css$/;
 // Pattern for image files
 const imagePattern = /\.(png|jpe?g|gif|svg|webp|avif)$/;
 
-
+// Export Vite configuration
 export default defineConfig(async () => {
-    // busca si no existen el certificado y la llave 
-    // para HTTPS
+    // Ensure the certificate and key exist
     if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-        // si no existe espera a que se genere el certificado
-        // espera a que se genere el certificado
+        // Wait for the certificate to be generated
         await new Promise<void>((resolve) => {
-                //spawn lo que hace es ejecutar el comando especificado entre ''
-                //creo que llo que hace es correr el proyecto con las credenciales
-                spawn('dotnet', [
-                    'devcerts',
-                    'https',
-                    '--export-path',
-                    certFilePath,
-                    '--format',
-                    'Pem',
-                    '--no-password'
-                ], { stdio: 'inherit', })
-                    //el on creo que checa cuando se termina de ejecutar el comando que se paso
-                    .on('exit', (code: any) => {
-                        resolve();
-                        if (code) {
-                            // esto termina el proceso de ejecucion de node 
-                            // con el resultado que obtenga de buscar los certificados
-                            process.exit(code);
-                        }
-                    });
+            spawn('dotnet', [
+                'dev-certs',
+                'https',
+                '--export-path',
+                certFilePath,
+                '--format',
+                'Pem',
+                '--no-password',
+            ], { stdio: 'inherit', })
+                .on('exit', (code) => {
+                    resolve();
+                    if (code) {
+                        process.exit(code);
+                    }
+                });
         });
     };
+
+    // Define Vite configuration
     const config: UserConfig = {
-        // react lel
-        plugins: [react()],
-        clearScreen: true,
-        appType: 'spa',
-        root: 'Client',
+        appType: 'custom',
+        root: 'Assets',
         publicDir: 'public',
         build: {
-            manifest: appsettings.Vite.Manifest,
+            manifest: true,
             emptyOutDir: true,
-            // donde va a scar la pagina yum yum
-            outDir: './wwwroot',
+            outDir: '../wwwroot',
             assetsDir: '',
             rollupOptions: {
-                input: ['Client/main.ts', "Client/scss/site.scss"],
-
-                // remove hashing??, viene en la guia pero no se que haga, pero lo dejare por si acaso :]
+                input: 'Assets/src/app/main.tsx',
                 output: {
-                    entryFileNames: 'js/[name].js',
+                    // Save entry files to the appropriate folder
+                    entryFileNames: 'js/[name].[hash].js',
+                    // Save chunk files to the js folder
                     chunkFileNames: 'js/[name]-chunk.js',
+                    // Save asset files to the appropriate folder
                     assetFileNames: (info) => {
                         if (info.name) {
                             // If the file is a CSS file, save it to the css folder
                             if (cssPattern.test(info.name)) {
                                 return 'css/[name][extname]';
                             }
-                            // Image file on images folder
+                            // If the file is an image file, save it to the images folder
                             if (imagePattern.test(info.name)) {
-                                return 'image/[name][extname]';
+                                return 'images/[name][extname]';
                             }
-                            // any other type
+
+                            // If the file is any other type of file, save it to the assets folder
                             return 'assets/[name][extname]';
                         } else {
-                            // lo salva en laprimera carpeta del output
-                            return '[name][extname]'
+                            // If the file name is not specified, save it to the output directory
+                            return '[name][extname]';
                         }
                     },
                 }
             },
         },
         server: {
-            //se especifia el puerto en el que se ejecuta Vite en appsettings.Development.json,
-            // se puede acceder si expandes appsettings.json en VS
-            port: appsettingsDev.Vite.Server.Port,
-            // solo acceso al puerto especificado, por si tienen error
-            // quizas un puerto ocupe el que Vite quiere
             strictPort: true,
             https: {
                 cert: certFilePath,
                 key: keyFilePath
-            },
-            hmr: {
-                host: 'localhost',
-                clientPort: appsettingsDev.Vite.Server.Port
             }
-        }
+        },
+        plugins: [
+            react()
+        ],
+        optimizeDeps: {
+            include: []
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: "modern-compiler",
+                },
+            },
+        },
     }
+
     return config;
 });
-
-// https://vite.dev/config/
-// esto es lo que viene por defecto
-// lo dejeo por si acaso aaa
-//export default defineConfig({
-//  plugins: [react()],
-//})
