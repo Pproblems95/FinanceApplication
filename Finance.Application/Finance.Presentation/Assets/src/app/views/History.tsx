405f6283e661461f9c708cbf7e7235670196884a
Pproblems95 <l21212001@tectijuana.edu.mx>
2026-07-14 19:11:43 -0700

    se trabajo en el GalleryItem, sigue el VerticalGallery

--- Finance.Application/Finance.Presentation/Assets/src/app/views/History.tsx
+++ Finance.Application/Finance.Presentation/Assets/src/app/views/History.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import PositiveNegativeButton from '../../shared/components/PositiveNegativeButton'
-import { CalendarIcon } from '../../shared/components/icons'
+import { CalendarIcon, MoneyIcon } from '../../shared/components/icons'
 import InputFilter from '../../shared/components/InputFilter'
+import GalleryItem from '../../shared/components/GalleryItem'
 
 function History() {
     return(
@@ -12,7 +13,10 @@ function History() {
         <PositiveNegativeButton text='Filter by outcome' isCurrentlySelected={false} backgroundColor='#CE311C' hoveredBackgroundColor='#f24822' selectedBackgroundColor='#1A7E21' onClick={() => {console.log('hi')}}/>
         
         <InputFilter  placeholder='From' type='date' backgroundColor='#151734' icon={<CalendarIcon size={20} color='#848FD3'/>}/>
-        
+        <GalleryItem icon={<MoneyIcon size={35} color='#1E9326'/>} title='Test' description='this is a test america fak yeah' 
+        titleColor='#848FD3' descriptionColor='#848FD3' backgroundColor='#12142D' display={<div className=' w-full h-full flex justify-center items-center'> 
+          <p  className='text-lg font-bold' style={{color:'#1E9326'}}>$2000</p>
+        </div>}/>
       </div>
     )
 }
--- Finance.Application/Finance.Presentation/Assets/src/shared/components/GalleryItem.tsx
+++ Finance.Application/Finance.Presentation/Assets/src/shared/components/GalleryItem.tsx
@@ -6,20 +6,22 @@ interface GalleryItemProps {
     title: string,
     description: string,
     display: React.ReactNode,
-    backgroundColor: string
+    backgroundColor: string,
+    titleColor: string,
+    descriptionColor: string
 };
 
-function GalleryItem({icon, title, description, display, backgroundColor}: GalleryItemProps){
+function GalleryItem({icon, title, description, display, backgroundColor, titleColor, descriptionColor}: GalleryItemProps){
     return(
-    <div style={{background: backgroundColor}}>
-      <div>
+    <div className=' flex flex-row rounded-lg p-2 ' style={{background: backgroundColor}}>
+      <div className=' w-full flex flex-1 items-center justify-center '>
         {icon}
       </div>
-      <div>
-        {title}
-        {description}
+      <div className=' flex flex-3 flex-col bg-danger'>
+        <span style={{color: titleColor}} className='font-bold text-2xl'>{title}</span>
+        <span style={{color:descriptionColor}} className='text-md'>{description}</span>
       </div>
-      <div>
+      <div className='flex flex-1'>
         {display}
       </div>
     </div>)
